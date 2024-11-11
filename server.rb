a72335aca778e36074cddaa04d61d0a503e39838
daisuke721 <d.magome.07@gmail.com>
2024-11-11 15:39:43 +0900

    refactor: 不要なコメント、コードの削除 #3

--- server.rb
+++ server.rb
@@ -1,17 +1,11 @@
 require 'webrick'
 require 'mysql2'
 
+# webコンテナ内で設定した環境変数を読み込みruby内で使用できるように変数に代入
 DB_HOST = ENV['DATABASE_HOST']
 DB_USER = ENV['DATABASE_USER']
 DB_PASSWORD = ENV['DATABASE_PASSWORD']
 DB_NAME = ENV['DATABASE_NAME']
-
-# client = Mysql2::Client.new(
-#   host: DB_HOST,
-#   username: DB_USER,
-#   password: DB_PASSWORD,
-#   database: DB_NAME
-# )
 
 server = WEBrick::HTTPServer.new(Port: 8000, DocumentRoot: './views')
 
