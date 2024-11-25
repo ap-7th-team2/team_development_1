19412c94728ffa8cb5778858cfba571ab009e015
Megumin32 <1.618ogon@gmail.com>
2024-11-25 23:59:50 +0900

    add: データベース構築 #7

---
+++ routes/snippet.rb
@@ -0,0 +1,31 @@
+require 'mysql2'
+
+DB_HOST = ENV['DATABASE_HOST']
+DB_USER = ENV['DATABASE_USER']
+DB_PASSWORD = ENV['DATABASE_PASSWORD']
+DB_NAME = ENV['DATABASE_NAME']
+
+begin
+  # MySQLサーバに接続
+  client = Mysql2::Client.new(
+    host: DB_HOST,   
+    username: DB_USER,          
+    password: DB_PASSWORD, 
+    database: DB_NAME    
+  )
+
+  puts "データベースに接続しました！"
+
+  # テーブル情報の取得
+  results = client.query("SHOW TABLES;")
+  puts "テーブル一覧:"
+  results.each do |row|
+    puts row
+  end
+
+rescue Mysql2::Error => e
+  puts "エラー: #{e.message}"
+ensure
+  client&.close
+  puts "接続を閉じました。"
+end
