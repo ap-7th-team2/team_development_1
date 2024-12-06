add8883bacaef8fa0a643a88ff37ebd4f6bac524
daisuke721 <d.magome.07@gmail.com>
2024-12-07 01:15:40 +0900

    add: sever.rbでモジュール化したsubmit_route.rbのエンドポイントでデータ処理 #15

--- server.rb
+++ server.rb
@@ -1,13 +1,12 @@
 require 'webrick'
-require 'mysql2'
-
-# webコンテナ内で設定した環境変数を読み込みruby内で使用できるように変数に代入
-# GitHub Actionsの動作確認2回目
-DB_HOST = ENV['DATABASE_HOST']
-DB_USER = ENV['DATABASE_USER']
-DB_PASSWORD = ENV['DATABASE_PASSWORD']
-DB_NAME = ENV['DATABASE_NAME']
+require_relative 'routes/submit_route'
 
 server = WEBrick::HTTPServer.new(Port: 8000, DocumentRoot: './views')
 
+# `/submit` エンドポイントでフォームデータを処理
+server.mount_proc '/submit' do |req, res|
+  SubmitRoute.handle_request(req, res)
+end
+
+trap('INT') { server.shutdown }
 server.start
